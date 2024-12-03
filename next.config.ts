78e3d8df5dd63f7023049dc0b1562c70ad4a6215
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2024-12-03 20:18:52 +0100

    fixed prod build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["i.ytimg.com", "yt3.ggpht.com", "s.ytimg.com"],
+    remotePatterns: ["i.ytimg.com", "yt3.ggpht.com", "s.ytimg.com"],
   },
 };
 
