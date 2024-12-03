591eefd74cd1d76457a1838e772b838633b14c91
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2024-12-03 20:44:02 +0100

    urgent fix 2

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["i.ytimg.com", "yt3.ggpht.com", "s.ytimg.com"],
   },
+  env: {
+    YOUTUBE_SEARCH_API_V3_KEY: process.env.YOUTUBE_SEARCH_API_V3_KEY,
+  }
 };
 
 module.exports = nextConfig;
