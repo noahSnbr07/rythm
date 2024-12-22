f4b70f177098e8531b9e85cb76763cf74bdd7440
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2024-12-22 18:41:10 +0100

    fix 10

--- db.ts
+++ db.ts
@@ -3,4 +3,4 @@ import postgres from "postgres";
 const connectionString = process.env.POSTGRES_URL || "";
 const sql: postgres.Sql = postgres(connectionString);
 
-export default sql;
+export default sql as postgres.Sql;
